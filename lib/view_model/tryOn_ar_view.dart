1f0dff3282854c3c895225a673bd86131c5cf161
Naveed-kk <naveedkaimkhami@gmail.com>
2025-08-09 12:41:57 -0700

    .

--- lib/view_model/tryOn_ar_view.dart
+++ lib/view_model/tryOn_ar_view.dart
@@ -1,18 +1,21 @@
-import 'package:deepar_flutter/deepar_flutter.dart';
+import 'package:deepar_flutter_plus/deepar_flutter_plus.dart';
 import 'package:flutter/material.dart';
 
 class TryOnArView extends StatefulWidget {
+  const TryOnArView({Key? key}) : super(key: key);
+
   @override
-  _TryOnArViewState createState() => _TryOnArViewState();
+  State<TryOnArView> createState() => _TryOnArViewState();
 }
 
 class _TryOnArViewState extends State<TryOnArView> {
-  late DeepArController _controller;
+  late DeepArControllerPlus _controller;
+  bool _isInitialized = false;
 
   @override
   void initState() {
     super.initState();
-    _controller = DeepArController();
+    _controller = DeepArControllerPlus();
     _initializeDeepAr();
   }
 
@@ -22,26 +25,31 @@ class _TryOnArViewState extends State<TryOnArView> {
       iosLicenseKey: '50d983dbef5c353680edf72212e3f4ed97da6e8b3aa7e0dfe86b9a2d97540e8ea8494e87e54efdbc',
       resolution: Resolution.high,
     );
-    setState(() {});
+    setState(() {
+      _isInitialized = true;
+    });
   }
 
   @override
   void dispose() {
-    // _controller.dispose();
+    // _controller.dispose(); // properly dispose controller
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _controller.isInitialized
-          ? DeepArPreview(_controller)
+      body: _isInitialized
+          ? DeepArPreviewPlus(_controller)
           : const Center(child: CircularProgressIndicator()),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.filter),
+        child: const Icon(Icons.filter),
         onPressed: () {
-          // Example to load a filter file from assets
-          _controller.switchEffect('assets/filters/burning_effect.deepar');
+          // Load your custom glasses filter from assets
+          _controller.switchEffect(
+            'assets/filters/Hope.deepar', // <-- replace with your .deepar file
+            // slot: 'mask', // slot type: mask, effect, filter
+          );
         },
       ),
     );
